c9f12abbb861506236203c3566065330345cb3de
dhirajs16 <sarrafd16@gmail.com>
2024-10-31 22:14:06 +0545

    Get and Post Operation

--- drf_react/frontend/src/App.jsx
+++ drf_react/frontend/src/App.jsx
@@ -1,20 +1,54 @@
-const App = () => {
-  const [data, setData] = useState([]);
+import axios from "axios";
+import { useState } from "react";
 
-  useEffect(() => {
-    axios
-      .get("http://127.0.0.1:8000/api/")
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
-        console.error("There was an error fetching data!", error);
-      });
-  }, []);
+const App = () => {
+  const [id, setId] = useState(0);
+
+  const handleGetAll = async () => {
+    const getData = await axios.get("http://127.0.0.1:8000/api/");
+    console.log(getData.data);
+  };
+
+  const handlePost = async () => {
+    const postData = {
+      name: "Shivam",
+      roll: 1011,
+      city: "Shivpur",
+    };
+    const response = await axios.post("http://127.0.0.1:8000/api/", postData);
+    console.log(response);
+  };
+
+  const handleGet = async (e) => {
+    e.preventDefault();
+    if (id) {
+      try {
+        const response = await axios.get(`http://127.0.0.1:8000/api/7`);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    } else {
+      console.error("Please enter a valid ID");
+    }
+  };
 
   return (
     <>
       <div>App</div>
+      <button onClick={handleGetAll}>Get All Data</button>
+      <button onClick={handlePost}>Post Data</button>
+      <hr />
+      <form onSubmit={handleGet}>
+        <input
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          type="number"
+          name="id"
+          id="id_id"
+        />
+        <button type="submit">Get Data</button>
+      </form>
     </>
   );
 };
